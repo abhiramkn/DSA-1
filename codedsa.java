71cf7c28fc85a3abc986a98fd09fd2ab3721dc33
Abhiram Chowdary Nalluri <komalabhiramnalluri06@gmail.com>
2026-07-10 08:45:10 +0530

    added dsa strings

---
+++ codedsa.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class codedsa{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        int count = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (i < s.length() - 1 && s.charAt(i) == s.charAt(i + 1)) {
+                count++;
+            } else {
+                System.out.print(s.charAt(i));
+                if (count > 1) {
+                    System.out.print(count);
+                }
+                count = 1;
+            }
+        }
+    }
+}
